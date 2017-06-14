8c4c86dc790d094695ce6eba76abdbf4b95ee468
hartred <artiomse@gmail.com>
2017-06-14 07:54:40 +0000

    small refactoring

--- TCPclient.py
+++ TCPclient.py
@@ -1,34 +1,35 @@
 import socket
 
 class Connection():
+    __host = None
+    __port = None
 
-    ip_address = property()
-    port_number = property()
+    def __init__(self, host, port):
+        self.__host = host
+        self.__port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # get и set ip-адреса
-    @ip_address.getter
-    def ip_address(self):
-        return self.__ip_address
+    @property
+    def host(self):
+        return self.__host
 
-    @ip_address.setter
-    def ip_address(self, ip_address):
-        self.__ip_address = ip_address
+    @host.setter
+    def host(self, ip_addr):
+        self.__host = ip_addr
 
 # get и set номера порта
-    @port_number.getter
-    def port_number(self):
-        return self.__port_number
+    @property
+    def port(self):
+        return self.__port
 
-    @port_number.setter
-    def port_number(self, port_number):
-        self.__port_number = port_number
+    @port.setter
+    def port(self, port_number):
+        self.__port = port_number
 
-    def begin_connection(self, ip_address, port_number):
-        self.sock = socket.socket()
-        self.__ip_address = ip_address
-        self.__port_number = port_number
+    def connect(self):
         try:
-            self.sock.connect((ip_address, port_number))
+            self.sock.connect((self.host, self.port))
             self.sock.timeout = 5
         except AttributeError:
             return 1
@@ -39,25 +40,17 @@ class Connection():
         else:
             return 0
 
-    def send_message(self, message_byte):
+    def send_data(self, data):
         try:
-            self.sock.send(message_byte)
+            self.sock.send(data)
         except AttributeError:
             return 1
         except Exception:
             return 2
         else:
             return 0
-
-    def close_connection(self):
-        try:
-            self.sock.close()
-        except AttributeError:
-            return 1
-        else:
-            return 0
-
-    def data_read(self):
+            
+    def receive_data(self):
         try:
             data = self.sock.recv(1024)
             print(type(data))
@@ -68,3 +61,10 @@ class Connection():
         else:
             return data
 
+    def close(self):
+        try:
+            self.sock.close()
+        except AttributeError:
+            return 1
+        else:
+            return 0
